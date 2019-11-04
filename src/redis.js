d20e088930a34343871167498acc42b09db7e4bd
Steven Kitzes <steven.kitzes@gmail.com>
2019-11-03 22:03:56 -0800

    feat: implement buyer endpoint and supporting redis backend to satisfy test cases

--- src/redis.js
+++ src/redis.js
@@ -2,4 +2,29 @@ var redis = process.env.NODE_ENV === 'test'
   ? require('fakeredis')
   : require('redis')
 
-module.exports = redis.createClient
+module.exports = {
+  setBuyer
+}
+
+const client = redis.createClient()
+client.on('connect', () => {
+  console.log('Redis local connection established')
+})
+client.on('error', (err) => {
+  console.log('Redis error:', err)
+})
+if (process.env.NODE_ENV !== 'test') client.flushall()
+
+function setBuyer (buyer, cb) {
+  try {
+    client.del(buyer.id)
+    buyer.offers.forEach(offer => {
+      client.rpush(buyer.id, JSON.stringify(offer))
+    })
+    client.rpush('buyers', buyer.id)
+    cb(null, {success: true})
+  } catch (exception) {
+    console.log('Exception caught in Redis setBuyer')
+    cb(exception)
+  }
+}
