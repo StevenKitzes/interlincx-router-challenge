9749a0f233ef35f1a8a3113272f6013d5e81be42
Steven Kitzes <steven.kitzes@gmail.com>
2019-11-05 02:17:04 -0800

    feat: implement /route endpoint to get highest value matching offer url

--- src/redis.js
+++ src/redis.js
@@ -4,7 +4,8 @@ var redis = process.env.NODE_ENV === 'test'
 
 module.exports = {
   setBuyer,
-  getBuyer
+  getBuyer,
+  getRoute
 }
 
 const client = redis.createClient()
@@ -48,4 +49,62 @@ function getBuyer (id, cb) {
       cb(null, buyer)
     })
   })
+}
+
+function getRoute (params, cb) {
+  client.lrange('buyers', 0, -1, (err, buyerKeyList) => {
+    if (err) {
+      cb(err)
+      return
+    }
+
+    processOffers(buyerKeyList, params, cb)
+  })
+}
+
+function processOffers (buyerKeyList, params, cb) {
+  const offers = []
+  let buyersProcessed = 0
+
+  if (buyerKeyList.length < 1) {
+    cb('not found')
+    return
+  }
+
+  buyerKeyList.forEach(key => {
+    client.lrange(key, 0, -1, (err, offerStrings) => {
+      if (err) return
+
+      offerStrings.forEach(offerString => {
+        offers.push(JSON.parse(offerString))
+      })
+
+      buyersProcessed++
+
+      if (buyersProcessed === buyerKeyList.length) {
+        sendBestOfferLocation(offers, params, cb)
+      }
+    })
+  })
+}
+
+function sendBestOfferLocation (offers, params, cb) {
+  const time = new Date(params.timestamp)
+  const targetHour = time.getUTCHours()
+  const targetDay = time.getUTCDay()
+  let highestValue = Number.MIN_SAFE_INTEGER
+  let location
+
+  offers.forEach(offer => {
+    if (offer.criteria.hour.indexOf(targetHour) < 0) return
+    if (offer.criteria.day.indexOf(targetDay) < 0) return
+    if (offer.criteria.state.indexOf(params.state) < 0) return
+    if (offer.criteria.device.indexOf(params.device) < 0) return
+    if (offer.value > highestValue) {
+      highestValue = offer.value
+      location = offer.location
+    }
+  })
+
+  cb(null, location)
 }
